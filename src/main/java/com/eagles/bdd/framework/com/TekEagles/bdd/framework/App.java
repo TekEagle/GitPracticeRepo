c08354c67c27a4b5e56b21f1b455b87df4a9d3d9
TekEagle <halimah.rafat@gmail.com>
2021-07-26 21:34:06 -0400

    this is practice code

---
+++ src/main/java/com/eagles/bdd/framework/com/TekEagles/bdd/framework/App.java
@@ -0,0 +1,13 @@
+package com.eagles.bdd.framework.com.TekEagles.bdd.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
